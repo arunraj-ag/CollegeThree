87549d901e0682d8277729f6ca3c4abe3fe69067
ArunRaj <arunraj8188@gmail.com>
2021-03-02 20:29:59 +0530

    Added signup form

--- app/src/main/java/com/example/college/Common/LoginSignup/StartUpScreen.java
+++ app/src/main/java/com/example/college/Common/LoginSignup/StartUpScreen.java
@@ -2,7 +2,11 @@ package com.example.college.Common.LoginSignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.example.college.R;
@@ -14,5 +18,20 @@ public class StartUpScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_up_screen);
+    }
+
+    public void callLoginScreen(View view){
+        Intent intent = new Intent(getApplicationContext(),Login.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View,String>(findViewById(R.id.login_startup_btn),"transition_login");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(StartUpScreen.this,pairs);
+            startActivity(intent,options.toBundle());
+        }
+        else
+            startActivity(intent);
+
     }
 }
